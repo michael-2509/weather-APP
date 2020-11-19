4fd6b901843335dfe9e0cdf738729522d091b697
michael-2509 <idowumichael2509@gmail.com>
2020-11-19 22:14:40 +0100

    updated weather icon

--- script.js
+++ script.js
@@ -1,25 +1,56 @@
-var card = document.getElementsByClassName('card')
-var input = document.querySelector('.name')
-var submit = document.getElementsByClassName('submit');
-var name = document.getElementsByClassName('name');
-var temp= document.getElementsByClassName('temp');
-var desc = document.getElementsByClassName('desc');
+var display = document.querySelector('.display');
+var test = document.getElementById('test');
+var input = document.querySelector('.inputValue')
+var submit = document.querySelector('.submit');
+var desire = document.querySelector('.desire');
+var temp= document.querySelector('.temp');
+var desc = document.querySelector('.desc');
+var iconn = document.querySelector('.icon');
+var image = document.createElement('img');
+var tick = document.querySelector('.tick-tock')
 
 
-card.addEventListener ('submit', function(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=1d19135b5e0c3aa77f8aa4dd056afed7')
+
+
+submit.addEventListener ('click', function() {
+    fetch('http://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=1d19135b5e0c3aa77f8aa4dd056afed7&units=metric')
     .then(response => response.json())
     .then(data => {
-      var tempValue = data['main']['temp'];
-      var nameValue = data['name'];
-      var descValue = data['weather'][0]['description'];
-    
-    name.innerHTML = nameValue;
-      desc.innerHTML = descValue;
-      temp.innerHTML = tempValue;
-    
-    
-    })
-    
 
+      var nameValue = data['name'];
+      var tempValue = data['main']['temp'];
+      var descValue = data['weather'][0]['description'];
+      var iconValue = data['weather'][0]['icon'];
+    
+      desire.innerHTML = nameValue ;
+      temp.innerHTML = Math.round(tempValue)+'&deg'+ 'C';
+      desc.innerHTML = descValue;
+      
+      image.setAttribute('src', "http://openweathermap.org/img/wn/"+iconValue+"@2x.png");
+      test.appendChild(image);
+  
+      
+
+      let now = new Date();
+      tick.innerHTML = update(now);
     })
+
+      function update (d) {
+      let months = ["January","Febuary","March","April","May","June","July","August","September","October","November","December"]
+      let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+      let day = days[d.getDay()];
+      let month = months[d.getMonth()];
+      let date = d.getDate();
+      let year = d.getFullYear();
+    
+      return `${day} ${date} ${month} ${year}`
+    }
+
+   
+   });
+
+
+
+
+   
